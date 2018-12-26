eb1e5c70471ecbc03e1dbdeb7dbf6d443a7f83f9
Niwesh Gupta <niweshgupta@gmail.com>
2018-12-26 18:21:44 +0530

    Multiple file saving option added

--- submit.php
+++ submit.php
@@ -1,9 +1,17 @@
 <html>
 <body>
 	<?php 
-	$file = fopen("file1.txt", "w");
+	$directory = "/var/www/html/";
+	$filecount = 0;
+	$files = glob($directory . "*.txt");
+	if ($files){
+ 		$filecount = count($files);
+	}
+	$filecount++;
+	$filename = "file".$filecount.".txt";
+	$file = fopen($filename, "w");
 	fwrite($file, $_POST["data"]);
-	echo "File saved successfully with name 'file1.txt'"; 
+	echo "File saved successfully with name $filename"; 
 	fclose($file);
 	?>
 
