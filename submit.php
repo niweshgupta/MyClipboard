8368a19aea3a7364c5d57a06fe9d8d9a6e2d4a38
Niwesh Gupta <niweshgupta@gmail.com>
2018-12-26 15:17:52 +0530

    Writing data to a file added

--- submit.php
+++ submit.php
@@ -1,5 +1,11 @@
 <html>
 <body>
-	<?php echo $_POST["data"]; ?>
+	<?php 
+	$file = fopen("file1.txt", "w");
+	fwrite($file, $_POST["data"]);
+	echo "File saved successfully with name 'file1.txt'"; 
+	fclose($file);
+	?>
+
 </body>
 </html>
