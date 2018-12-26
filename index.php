5b7ed82ed22992c06a553c70cf7eb25c86750a90
Niwesh Gupta <niweshgupta@gmail.com>
2018-12-26 22:09:37 +0530

    Added design and show file

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Default Page</title>
+  <title>My Clipboard - Niwesh Gupta</title>
 </head>
 <body>
-  <form action="submit.php" method="POST">
-    <input type="text" name="data" style="width: 800px; height: 500px">
-    <br> <br>
-    <input type="submit" value ="Save">
-  </form>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<h1 style="color: white; padding: 40px; text-align: center; background: #1abc9c;"> My Clipboard </h1>
+	<h2> Empty your clipboard here : </h2>
+  	<form action="submit.php" method="POST">
+    	<textarea name="data" style="width: 892px; height: 228px; align-self: center;"> </textarea>
+    	<br> <br>
+    	<input type="submit" value ="Save">
+  	</form>
+  	<br>
+  	<center>
+	  	<fieldset style="width: 200px;">
+		  	<legend> Contact Me: </legend>
+		  	<a href="http://www.facebook.com/niweshgupta" class="fa fa-facebook" style="padding: 10px;"></a>
+		  	<a href="http://www.twitter.com/niweshgupta" class="fa fa-twitter" style="padding: 10px;"></a>
+		  	<a href="http://www.instagram.com/niweshgupta" class="fa fa-instagram" style="padding: 10px;"></a>
+	  	</fieldset>
+  		
+  	</center>
 </body>
 </html>
